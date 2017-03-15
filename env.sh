f611df79063334d3619f486527e58fb32ddbf76a
Kristin Blacklock (Case 2013) black127@mail.chapman.edu <kristin.blacklock@rutgers.edu>
2017-03-15 14:53:00 -0400

    shuffled things, added env.sh script

---
+++ env.sh
@@ -0,0 +1,3 @@
+export ROSETTA=/home/kmb413/RealRosetta/Rosetta/main/source/bin/rosetta_scripts.default.linuxclangrelease
+export SCRIPTS=/home/kmb413/Azo_Elliott_Assembly/scripts
+export OUTPUT=/home/kmb413/Azo_Elliott_Assembly/output
---
+++ scripts/score.sh
@@ -0,0 +1,1 @@
+$ROSETTA -parser:protocol $SCRIPTS/score.xml -s $1 -out:path:pdb $OUTPUT @$SCRIPTS/flagsfile
